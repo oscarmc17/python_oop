862f7b0fd91285bc0b67d499c32d15b3a2c997a7
Oscar Caicedo <oscarccd17@gmail.com>
2022-07-19 21:37:38 -0500

    Inheritance

--- Videos_and_work/oop_video.py
+++ Videos_and_work/oop_video.py
@@ -1,91 +1,101 @@
-# # OOP Tutorial 7.14.22
-# import csv
+# # OOP Tutorial 7.18.22
+import csv
 
-# class Item:
-#     pay_rate = 0.8 #The pay rate after 20% discount.
-#     all = []
+class Item:
+    pay_rate = 0.8 #The pay rate after 20% discount.
+    all = []
 
-#     def __init__(self, name: str, price: float, quantity=0):
-#         # Run validations to the received arguments
-#         assert price >= 0, f"Price {price} is not greater than or equal to zero!"
-#         assert quantity >= 0, f"Quantity {quantity} is not greater or equal to zero!"
+    def __init__(self, name: str, price: float, quantity=0):
+        # Run validations to the received arguments
+        assert price >= 0, f"Price {price} is not greater than or equal to zero!"
+        assert quantity >= 0, f"Quantity {quantity} is not greater or equal to zero!"
         
-#         # Assign to self object
-#         self.name = name
-#         self.price = price
-#         self.quantity = quantity
+        # Assign to self object
+        self.name = name
+        self.price = price
+        self.quantity = quantity
 
-#         # Actions to execute
-#         Item.all.append(self)
+        # Actions to execute
+        Item.all.append(self)
         
 
-#     def calculate_total_price(self):
-#         return self.price * self.quantity
+    def calculate_total_price(self):
+        return self.price * self.quantity
     
-#     def apply_discount(self):
-#         self.price = self.price * self.pay_rate
+    def apply_discount(self):
+        self.price = self.price * self.pay_rate
     
-#     @classmethod
-#     def instantiate_from_csv(cls):
-#         with open('items.csv', 'r') as f:
-#             reader = csv.DictReader(f)
-#             items = list(reader)
+    @classmethod
+    def instantiate_from_csv(cls):
+        with open('items.csv', 'r') as f:
+            reader = csv.DictReader(f)
+            items = list(reader)
 
-#         for item in items:
-#             Item(
-#                 name = item.get('name'),
-#                 price = float(item.get('price')),
-#                 quantity = int(item.get('quantity')),
-#             )
+        for item in items:
+            Item(
+                name = item.get('name'),
+                price = float(item.get('price')),
+                quantity = int(item.get('quantity')),
+            )
 
-#     @staticmethod
-#     def is_integer(num):
-#         # We will count out the floats that are point zero. i.e: 5.0, 10.0.
-#         if isinstance(num, float):
-#             # Count out the floats that are point zero
-#             return num.is_integer()
-#         elif isinstance(num, int):
-#             return True
-#         else:
-#             return False
+    @staticmethod
+    def is_integer(num):
+        # We will count out the floats that are point zero. i.e: 5.0, 10.0.
+        if isinstance(num, float):
+            # Count out the floats that are point zero
+            return num.is_integer()
+        elif isinstance(num, int):
+            return True
+        else:
+            return False
         
 
 
-#     def __repr__(self):
-#         return f"Item('{self.name}', {self.price}, {self.quantity})"
+    def __repr__(self):
+        return f"Item('{self.name}', {self.price}, {self.quantity})"
 
 
 
-# print(Item.is_integer(7.0))
+    # print(Item.is_integer(7.0))
 
 
+# INHERITANCE
+class Phone(Item):
+     def __init__(self, name: str, price: float, quantity=0):
+        # Run validations to the received arguments
+        assert price >= 0, f"Price {price} is not greater than or equal to zero!"
+        assert quantity >= 0, f"Quantity {quantity} is not greater or equal to zero!"
+        
+        # Assign to self object
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+
+        # Actions to execute
+        Item.all.append(self)
+
+
+
+
+phone1 = Phone("jscPhonev10", 500, 5)
+phone1.broken_phones = 1
+phone2 = Phone("jscPhonev20", 700, 5)
+phone2.broken_phones = 1
 
 
 
 # When to use class methods and when to use statisc methods?
 
-class Item:
-    @staticmethod
-    def is_integer(num):
+# class Item:
+#     @staticmethod
+#     def is_integer(num):
         
-    #    This should do something that has a relationship with a class,
-    #    but not something that must be unique per instance!
+#     #    This should do something that has a relationship with a class,
+#     #    but not something that must be unique per instance!
 
-        @classmethod
-        def instance_from_something(cls):
-            # This should do something that has a relationship with the class,
-            # but usually, those are used to manipulate different structures
-            # of data to instantiate objects, like we have done with CSV.
-            pass
-
-
-
-# INHERITANCE
-import csv
-
-class Item:
-
-phone1 = Item("jscPhonev10", 500, 5)
-broken1.broken_phones = 1
-phone2 = Item("jscPhonev20", 700, 5)
-broken1.broken_phones = 1
+#         @classmethod
+#         def instance_from_something(cls):
+#             # This should do something that has a relationship with the class,
+#             # but usually, those are used to manipulate different structures
+#             # of data to instantiate objects, like we have done with CSV.
+#             pass
