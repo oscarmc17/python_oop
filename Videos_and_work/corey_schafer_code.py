a1dbf5a923e9d77f4e7350ad8abbf05d6324a081
Oscar Caicedo <oscarccd17@gmail.com>
2022-07-22 21:16:26 -0500

    more notes and progress

--- Videos_and_work/corey_schafer_code.py
+++ Videos_and_work/corey_schafer_code.py
@@ -51,16 +51,4 @@ dev_2 = Developer('Test', 'Employee', 60000, 'Java')
 
 mgr_1 = Manager('Sue', 'Smith', 90000, [dev_1])
 
-print(mgr_1.email)
-
-mgr_1.add_emp(dev_2)
-mgr_1.remove_emp(dev_1)
-
-mgr_1.print_emps()
-
-# print(dev_1.email)
-# print(dev_2.prog_lang)
-
-# print(dev_1.pay)
-# dev_1.apply_raise()
-# print(dev_1.pay)
+print(issubclass(Developer, Employee))
