0299546f6ba8698fc56a7286968e2faa50baf077
Oscar Caicedo <oscarccd17@gmail.com>
2022-07-27 16:38:31 -0500

    schafer @property setter

--- Videos_and_work/corey_schafer_code.py
+++ Videos_and_work/corey_schafer_code.py
@@ -1,54 +1,73 @@
 # COREY SCHAFER VIDEO PRAGRAMMING WITH PYTHON
 
-class Employee:
-    raise_amt = 1.04
+# class Employee:
+#     raise_amt = 1.04
 
-    def __init__(self, first, last, pay):
+#     def __init__(self, first, last, pay):
+#         self.first = first
+#         self.last = last
+#         self.email = first + '.' + last + '@email.com'
+#         self.pay = pay
+
+#     def fullname(self):
+#         return '{} {}'.format(self.first, self.last)
+
+#     def apply_raise(self):
+#         self.pay = int(self.pay * self.raise_amt)
+
+# class Developer(Employee):
+#     raise_amt = 1.10
+
+#     def __init__(self, first, last, pay, prog_lang):
+#         super().__init__(first, last, pay)
+#         self.prog_lang = prog_lang
+
+# class Manager(Employee):
+
+#     def __init__(self, first, last, pay, employees=None):
+#         super().__init__(first, last, pay)
+#         if employees is None:
+#             self.employees = []
+#         else:
+#             self.employees = employees
+        
+#     def add_emp(self, emp):
+#         if emp not in self.employees:
+#             self.employees.append(emp)
+    
+#     def remove_emp(self, emp):
+#         if emp in self.employees:
+#             self.employees.remove(emp)
+    
+#     def print_emps(self):
+#         for emp in self.employees:
+#             print('-->', emp.fullname())
+
+# dev_1 = Developer('Corey', 'Schafer', 50000, 'Python')
+# dev_2 = Developer('Test', 'Employee', 60000, 'Java')
+
+# mgr_1 = Manager('Sue', 'Smith', 90000, [dev_1])
+
+# print(issubclass(Developer, Employee))
+
+
+class Employee:
+
+    def __init__(self, first, last):
         self.first = first
         self.last = last
-        self.email = first + '.' + last + '@email.com'
-        self.pay = pay
+        
+    @property
+    def email(self):
+        return '{} {}@email.com'.format(self.first, self.last)
 
     def fullname(self):
         return '{} {}'.format(self.first, self.last)
-
-    def apply_raise(self):
-        self.pay = int(self.pay * self.raise_amt)
-
-
-class Developer(Employee):
-    raise_amt = 1.10
-
-    def __init__(self, first, last, pay, prog_lang):
-        super().__init__(first, last, pay)
-        self.prog_lang = prog_lang
-
-class Manager(Employee):
-
-    def __init__(self, first, last, pay, employees=None):
-        super().__init__(first, last, pay)
-        if employees is None:
-            self.employees = []
-        else:
-            self.employees = employees
-        
-    def add_emp(self, emp):
-        if emp not in self.employees:
-            self.employees.append(emp)
     
-    def remove_emp(self, emp):
-        if emp in self.employees:
-            self.employees.remove(emp)
-    
-    def print_emps(self):
-        for emp in self.employees:
-            print('-->', emp.fullname())
-        
+emp_1 = Employee('John', 'Smith')
 
+emp_1.first = "Jim"
 
-dev_1 = Developer('Corey', 'Schafer', 50000, 'Python')
-dev_2 = Developer('Test', 'Employee', 60000, 'Java')
-
-mgr_1 = Manager('Sue', 'Smith', 90000, [dev_1])
-
-print(issubclass(Developer, Employee))
+print(emp_1.first)
+print(emp_1.email)
+print(emp_1.fullname())
