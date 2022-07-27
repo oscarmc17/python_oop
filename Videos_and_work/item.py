9cb5bffac8ebd86266682f7d140f0481b04b2327
Oscar Caicedo <oscarccd17@gmail.com>
2022-07-27 13:49:00 -0500

    raising Exceptions()

--- Videos_and_work/item.py
+++ Videos_and_work/item.py
@@ -20,7 +20,15 @@ class Item:
     @property
     # Property Decorator = Read-Only Attribute
     def name(self):
+        print("You are trying to get name.")
         return self.__name
+    
+    @name.setter
+    def name(self, value):
+        if len(value) > 10:
+            raise Exception("The name is too long!")
+        else: 
+            self.__name = value
 
     def calculate_total_price(self):
         return self.price * self.quantity
--- Videos_and_work/oop_video.py
+++ Videos_and_work/oop_video.py
@@ -1,8 +1,11 @@
 # # OOP Tutorial 7.26.22
 
 from item import Item
-# from phone import Phone
 
 item1 = Item("MyItem", 750)
 
-item1.name = "OtherItem"
+# Setting an Attribute
+item1.name = "OtherItemmm"
+
+# Getting an Attribute
+print(item1.name)
